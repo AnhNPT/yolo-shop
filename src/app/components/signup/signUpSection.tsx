4c65c2cd80b5cee2da8e65af572405b5d1d6fae4
Emily <anhnpt2000@gmail.com>
2023-07-07 13:59:07 +0700

    add redux

--- src/app/components/signup/signUpSection.tsx
+++
@@ -1,7 +0,0 @@
-export default function SignUpSection() {
-    return(
-        <div className="w-1/2 h-screen">
-            <div className="bg-[#1f2128] z-30">abc</div>
-        </div>
-    )
-}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -8,7 +8,8 @@ import Image from "next/image";
 
 type Inputs = {
     fName: string;
-    exampleRequired: string;
+    eMail: string;
+    password: string;
 };
 
 export default function SignUp() {
@@ -22,6 +23,7 @@ export default function SignUp() {
     // State
     const [backgroundColorHex, setBackgroundColorHex] = useState<string>("");
     const [halfBgImage, checkHalfBgImage] = useState<string>("");
+    const [eyeImg, toggleEyeImg] = useState<boolean>(false);
     // Ref
     const elementRef = useRef<HTMLDivElement>(null);
     const halfBgRef = useRef<HTMLDivElement>(null);
@@ -71,52 +73,54 @@ export default function SignUp() {
                         <Link className="text-Y-yellow Y-header underline" href={"#"}>
                             Sign In
                         </Link>
-                        <form onSubmit={handleSubmit(onSubmit)} className="mt-12 flex flex-col gap-6">
-                            {/* FName */}
-                            <div className="flex flex-col w-1/2">
-                                <label className="text-Y-grey-1 Y-small_text mb-2">Your full name</label>
-                                <input
-                                    className="iconFname relative Y-text outline-none bg-[#444A5E] border-[#444A5E] border rounded-[40px] pl-12 px-4 py-3"
-                                    type="text"
-                                    defaultValue=""
-                                    {...register("fName", {
-                                        required: true,
-                                    })}
-                                />
-                                {errors.fName && <span className="mt-2 block Y-small_text text-Y-red">This field is required</span>}
-                            </div>
-                            {/* Email */    }
-                            <div className="flex flex-col w-1/2">
-                                <label className="text-Y-grey-1 Y-small_text mb-2">Your full name</label>
-                                <input
-                                    className="iconFname relative Y-text outline-none bg-[#444A5E] border-[#444A5E] border rounded-[40px] pl-12 px-4 py-3"
-                                    type="text"
-                                    defaultValue=""
-                                    {...register("fName", {
-                                        required: true,
-                                    })}
-                                />
-                                {errors.fName && <span className="mt-2 block Y-small_text text-Y-red">This field is required</span>}
-                            </div>
-                            {/* Password */}
-                            <div className="flex flex-col w-1/2">
-                                <label className="text-Y-grey-1 Y-small_text mb-2">Your full name</label>
-                                <input
-                                    className="iconFname relative Y-text outline-none bg-[#444A5E] border-[#444A5E] border rounded-[40px] pl-12 px-4 py-3"
-                                    type="text"
-                                    defaultValue=""
-                                    {...register("fName", {
-                                        required: true,
-                                    })}
-                                />
-                                {errors.fName && <span className="mt-2 block Y-small_text text-Y-red">This field is required</span>}
-                            </div>
-                            <button className="cursor-pointer rounded-[40px] border-2 bg-Y-yellow border-Y-black flex items-center px-4 py-3 w-fit gap-4">
-                                <span className="uppercase Y-button text-Y-dark-mode">sign up</span>
-                                <Image className="" alt="" src={"/images/big-arrow-right.svg"} width={24} height={1}></Image>
-                            </button>
-                        </form>
                     </span>
+                    <form onSubmit={handleSubmit(onSubmit)} className="mt-12 flex flex-col gap-6">
+                        {/* FName */}
+                        <div className="flex flex-col w-1/2">
+                            <label className="text-Y-grey-1 Y-small_text mb-2">Your full name</label>
+                            <input
+                                className="iconFname relative Y-text outline-none text-white bg-[#444A5E] border-[#444A5E] border rounded-[40px] pl-12 px-4 py-3"
+                                type="text"
+                                defaultValue=""
+                                {...register("fName", {
+                                    required: true,
+                                })}
+                            />
+                            {errors.fName && <span className="mt-2 block Y-small_text text-Y-red">Please enter your full name</span>}
+                        </div>
+                        {/* Email */}
+                        <div className="flex flex-col w-1/2">
+                            <label className="text-Y-grey-1 Y-small_text mb-2">Your email</label>
+                            <input
+                                className="iconEmail relative Y-text outline-none text-white bg-[#444A5E] border-[#444A5E] border rounded-[40px] pl-12 px-4 py-3"
+                                type="text"
+                                defaultValue=""
+                                {...register("eMail", {
+                                    required: true,
+                                    pattern: /([a-z0-9]+@[a-z]+\.[a-z]{2,3})/g,
+                                })}
+                            />
+                            {errors.eMail && <span className="mt-2 block Y-small_text text-Y-red">Please enter your email</span>}
+                        </div>
+                        {/* Password */}
+                        <div className="flex flex-col w-1/2">
+                            <label className="text-Y-grey-1 Y-small_text mb-2">Your password</label>
+                            <input
+                                className="iconFname relative Y-text outline-none text-white bg-[#444A5E] border-[#444A5E] border rounded-[40px] pl-12 px-4 py-3"
+                                type="password"
+                                defaultValue=""
+                                {...register("password", {
+                                    required: true,
+                                    pattern: /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[.@!#$%&]).{6,}$/g,
+                                })}
+                            />
+                            {errors.password && <span className="mt-2 block Y-small_text text-Y-red">Requires a password of more than 6 characters, including numbers and special characters</span>}
+                        </div>
+                        <button className="cursor-pointer rounded-[40px] border-2 bg-Y-yellow border-Y-black flex items-center px-4 py-3 w-fit gap-4">
+                            <span className="uppercase Y-button text-Y-dark-mode">sign up</span>
+                            <Image className="" alt="" src={"/images/big-arrow-right.svg"} width={24} height={1}></Image>
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
